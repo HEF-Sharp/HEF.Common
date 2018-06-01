1431479f194865cd83077c1e90e2fa8aab7af6b7
万里涛 <wanlitao_3055601@outlook.com>
2018-06-01 20:23:54 +0800

    add util extension methods

---
+++ src/HEF.Util/Extensions/StringExtensions.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Text;
+
+namespace HEF.Util
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// 判断空字符串
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static bool IsNullOrEmpty(this string str)
+        {
+            return (str ?? "").Length == 0;
+        }
+
+        #region Base64转换
+        /// <summary>
+        /// 转换为Base64字符串
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static string ToBase64String(this string str)
+        {
+            return str.ToBase64String("utf-8");
+        }
+
+        /// <summary>
+        /// 转换为Base64字符串
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static string ToBase64String(this string str, string encodeName)
+        {
+            if (str.IsNullOrEmpty())
+                return string.Empty;
+
+            var bytes = Encoding.GetEncoding(encodeName).GetBytes(str);
+
+            return Convert.ToBase64String(bytes);
+        }
+
+        /// <summary>
+        /// 解密Base64字符串
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static string FromBase64String(this string str)
+        {
+            return str.FromBase64String("utf-8");
+        }
+
+        /// <summary>
+        /// 解密Base64字符串
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static string FromBase64String(this string str, string encodeName)
+        {
+            if (str.IsNullOrEmpty())
+                return string.Empty;
+
+            var bytes = Convert.FromBase64String(str);
+
+            return Encoding.GetEncoding(encodeName).GetString(bytes);
+        }
+        #endregion
+    }
+}
---
+++ src/HEF.Util/Extensions/TypeExtensions.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace HEF.Util
+{
+    public static class TypeExtensions
+    {
+        /// <summary>
+        /// 是否为具体类型
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public static bool IsConcrete(this Type type)
+        {
+            return type.IsClass && !type.IsAbstract;
+        }
+
+        /// <summary>
+        /// 是否继承特定类型
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public static bool Is<T>(this Type type)
+        {
+            return type.Is(typeof(T));
+        }        
+
+        /// <summary>
+        /// 是否继承特定类型
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="baseType"></param>
+        /// <returns></returns>
+        public static bool Is(this Type type, Type baseType)
+        {
+            if (baseType == null)
+                throw new ArgumentNullException(nameof(baseType));
+
+            if (type == null)
+                return false;
+
+            if (baseType.IsAssignableFrom(type))
+                return true;
+
+            if (type == typeof(object))
+                return false;
+
+            if (baseType.IsGenericTypeDefinition &&
+                type.IsGenericType && type.GetGenericTypeDefinition() == baseType)
+                return true;
+
+            var interfaceTypes = type.GetInterfaces();
+            foreach (var interfaceType in interfaceTypes)
+            {
+                if (interfaceType.Is(baseType))
+                    return true;
+            }
+
+            return type.BaseType.Is(baseType);
+        }
+    }
+}
