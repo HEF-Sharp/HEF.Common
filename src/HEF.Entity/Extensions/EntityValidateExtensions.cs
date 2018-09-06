6f2eadf1e1dfd1b55b563f43eb1c493195282c8c
万里涛 <wanlitao_3055601@outlook.com>
2018-09-06 20:52:07 +0800

    add entity validation

---
+++ src/HEF.Entity/Extensions/EntityValidateExtensions.cs
@@ -0,0 +1,75 @@
+﻿using HEF.Util;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace HEF.Entity
+{
+    public static class EntityValidateExtensions
+    {
+        public static ICollection<ValidationResult> Validate<TEntity>(this TEntity entity,
+            params Expression<Func<TEntity, object>>[] ignorePropertyExpressions)
+        {
+            if (entity == null)
+                throw new ArgumentNullException(nameof(entity));
+
+            var propertyNames = GetPropertyNamesByExpression(ignorePropertyExpressions);
+
+            return ValidateInternal(entity, (name) => !propertyNames.Contains(name));
+        }
+
+        public static ICollection<ValidationResult> ValidateInclude<TEntity>(this TEntity entity,
+            params Expression<Func<TEntity, object>>[] includePropertyExpressions)
+        {
+            if (entity == null)
+                throw new ArgumentNullException(nameof(entity));
+
+            if (includePropertyExpressions.IsEmpty())
+                throw new ArgumentNullException(nameof(includePropertyExpressions));
+
+            var propertyNames = GetPropertyNamesByExpression(includePropertyExpressions);
+
+            return ValidateInternal(entity, (name) => propertyNames.Contains(name));
+        }
+
+        private static ICollection<ValidationResult> ValidateInternal<TEntity>(TEntity entity, Func<string, bool> propertyPredicate)
+        {
+            var (isValid, validationResults) = ValidateInternal(entity);
+            if (isValid)
+                return validationResults;
+
+            return validationResults.Where(m => propertyPredicate(m.MemberNames.FirstOrDefault())).ToList();
+        }
+
+        private static (bool, ICollection<ValidationResult>) ValidateInternal<TEntity>(TEntity entity)
+        {
+            var validationResults = new Collection<ValidationResult>();
+
+            var validationContext = new ValidationContext(entity, null, null);
+            var isValid = Validator.TryValidateObject(entity, validationContext, validationResults, true);
+
+            return (isValid, validationResults);
+        }
+
+        private static IEnumerable<string> GetPropertyNamesByExpression<T>(params Expression<Func<T, object>>[] propertyExpressions)
+        {
+            if (propertyExpressions.IsNotEmpty())
+            {
+                foreach (var propertyExpression in propertyExpressions)
+                {
+                    if (propertyExpression == null)
+                        continue;
+
+                    var property = propertyExpression.ParseProperty();
+                    if (property == null)
+                        continue;
+
+                    yield return property.Name;
+                }
+            }
+        }
+    }
+}
---
+++ src/HEF.Entity/Extensions/ExpressionExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq.Expressions;
+using System.Reflection;
+
+namespace HEF.Entity
+{
+    internal static class ExpressionExtensions
+    {
+        /// <summary>
+        /// 从Lambda表达式中获取PropertyInfo
+        /// </summary>
+        /// <param name="lambda"></param>
+        /// <returns></returns>
+        internal static PropertyInfo ParseProperty(this LambdaExpression lambda)
+        {
+            Expression expr = lambda;
+            for (; ; )
+            {
+                switch (expr.NodeType)
+                {
+                    case ExpressionType.Lambda:
+                        expr = ((LambdaExpression)expr).Body;
+                        break;
+                    case ExpressionType.Convert:
+                        expr = ((UnaryExpression)expr).Operand;
+                        break;
+                    case ExpressionType.MemberAccess:
+                        MemberExpression memberExpression = (MemberExpression)expr;
+                        return memberExpression.Member as PropertyInfo;
+                    default:
+                        return null;
+                }
+            }
+        }
+    }
+}
