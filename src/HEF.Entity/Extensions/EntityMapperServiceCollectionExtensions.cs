9aab57c6f3df6aafcc75d5d25a0dba683165420d
万里涛 <wanlitao_3055601@outlook.com>
2020-03-12 15:07:46 +0800

    add entity mapper servicecollection extensions

---
+++ src/HEF.Entity/Extensions/EntityMapperServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using HEF.Entity.Mapper;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class EntityMapperServiceCollectionExtensions
+    {
+        public static IServiceCollection AddEntityMapperProvider(this IServiceCollection serviceCollection,
+            Type defaultEntityMapper)
+        {
+            serviceCollection.TryAddSingleton<IEntityMapperProvider>(
+                (provider) => new EntityMapperProvider(defaultEntityMapper));
+
+            return serviceCollection;
+        }
+    }
+}
